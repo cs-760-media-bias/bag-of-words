5135c87dd2ae513c3374edb36acae70b9415b5fe
Katie Mummah <nuclearkatie@gmail.com>
2019-05-04 13:39:34 -0500

    changing column formatting

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ df = importTidy('data/tweets_tidy/', 'data/sources.json')
 
 #If only a random subset of the data is to be used, choose the fractional
 #size here, i.e. 25% would lead to frac=0.25
-simplified = df.sample(frac=1.0)
+simplified = df.sample(frac=0.10)
 simplified = simplified.reset_index()
 
 print('Cleaning tweets...')
@@ -32,13 +32,16 @@ print('Joining BOW data with tweet metadata...')
 bow_df = pd.DataFrame(bag_of_words.toarray(), columns=feature_names)
 bow_ed = pd.concat([simplified, bow_df], axis=1, sort=False)
 
-y= bow_ed["source"]
-X = bow_ed.drop(["id", "source", "reply_to_screen_name", "reply_to_tweet_id", "reply_to_user_id", "text", "urls", "user_mentions", "hashtags", "tweetTime", "index"], axis=1)
+#y= bow_ed["tweetSource"]
+y = bow_ed[["ad_fontes_x", "ad_fontes_y", "retweet_count", "favorite_count"]]
+X = bow_ed.drop(["id", "tweetSource", "reply_to_screen_name", "reply_to_tweet_id", "reply_to_user_id", "text", "urls", "user_mentions", "hashtags", "tweetTime", "index"], axis=1)
 
 print('Train/test splitting...')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 print('Writing data...')
+if not os.path.exists('preprocessed'):
+    os.makedirs('preprocessed')
 X_train.to_csv('preprocessed/X_train.csv', sep=',', header=True)
 X_test.to_csv('preprocessed/X_test.csv', sep=',', header=True)
 y_train.to_csv('preprocessed/y_train.csv', sep=',', header=True)
--- utility.py
+++ utility.py
@@ -29,7 +29,13 @@ def importTidy(path_to_tweets, path_to_bias):
         with open('data/tweets_tidy/'+file, 'r') as f:
             datastore = json.load(f)
             df_new = pd.DataFrame(datastore["tweets"])
-            df_new["tweetSource"]=file.rsplit(".")[0]
+            #df_new["tweetSource"]=file.rsplit(".")[0]
+            df_new["tweetSource"]=datastore["user"]["screen_name"]
+            df_new["followers_count"]=datastore["user"]["followers_count"]
+            df_new["friends_count"]=datastore["user"]["friends_count"]
+            df_new["listed_count"]=datastore["user"]["listed_count"]
+            df_new["statuses_count"]=datastore["user"]["statuses_count"]
+
             for source in bias["sources"]:
                 if file.rsplit(".")[0] in source["twitter_handles"]:
                     df_new["ad_fontes_x"] = source["ad_fontes_x"]
@@ -37,10 +43,19 @@ def importTidy(path_to_tweets, path_to_bias):
 
             df=df.append(df_new, ignore_index=True)
 
+    #split up datetime
     df = datetime(df)
+    #ensure numeric columns are numeric and not string
     df = numericColumns(df)
+    #map month and weekday to numeric representation
+    df = mapDate(df)
+
+    #move ad_fontes_x/y to front
+    df = df[['ad_fontes_x', 'ad_fontes_y'] +
+       [c for c in df if c not in ['ad_fontes_x', 'ad_fontes_y']]]
+
+    #get count of hashtags
     df["hashtag_count"]=df.apply(lambda row: len(row.hashtags), axis=1)
-    df['tweetWeekday'].value_counts()
 
     return df
 
@@ -74,6 +89,33 @@ def datetime(df):
     cols_in_front = ["id", "tweetWeekday", "tweetMonth", "tweetDayOfMonth", "tweetTime", "tweetYear"]
     df = df[[c for c in cols_in_front if c in df]
        + [c for c in df if c not in cols_in_front]]
+
+    return df
+
+
+def mapDate(df):
+    '''
+    df = mapDate(df)
+
+    Maps the month and day-of-week columns to their numeric equivalent. Sunday=1
+
+    Columns that are converted:
+        - tweetWeekday
+        - tweetMonth
+
+    input:
+        - df: a pandas dataframe containing the columns to be converted
+
+    output:
+        - df: the same pandas dataframe after conversion
+    '''
+
+    mo = {'JAN':1, 'FEB':2, 'MAR':3, 'APR':4, 'MAY':5, 'JUN':6, 'JUL':7,
+          'AUG':8, 'SEP':9, 'OCT':10, 'NOV':11, 'DEC':12}
+    wd = {'SUN':1, 'MON':2, 'TUE':3, 'WED':4, 'THU':5, 'FRI':6, 'SAT':7}
+
+    df.tweetMonth = df.tweetMonth.str.upper().map(mo)
+    df.tweetWeekday = df.tweetWeekday.str.upper().map(wd)
 
     return df
 
